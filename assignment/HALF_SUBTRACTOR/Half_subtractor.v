d3f6f58cc85b2a93914f14a20f6557de7e6efe6e
𝙎𝙀𝙀𝙉𝙄𝙑𝘼𝙎𝘼𝙉 <seenivasanv222@gmail.com>
2025-11-01 22:08:23 +0530

    Update Half_subtractor.v

--- assignment/HALF_SUBTRACTOR/Half_subtractor.v
+++ assignment/HALF_SUBTRACTOR/Half_subtractor.v
@@ -34,23 +34,54 @@ module  half_subtractor(
   
   always@(*) begin
     case({a,b})
-      2'b00:
+        2'b00:
       		begin
-              diff=0;borrow=0;
-            end
+             	diff=0;borrow=0;
+           		 end
       2'b01:
       		begin
-              diff=1;borrow=1;
-            end
+              	diff=1;borrow=1;
+            	 end
      2'b10:
       		begin
               diff=1;borrow=0;
-            end
+               end
       2'b11:
        		begin
               diff=0;borrow=0;
-            end
+               end
     endcase
   end
-   endmodule
+endmodule
+
+--------------------------------------//functional level-coding//-------------------------------------------------------
+module half_subtractor(
+  			input a,b,
+  			output diff,borrow
+);
+  
+  function[1:0]half_sub;
+    input a,b;
+    begin
+    
+    half_sub[0]=a^b;
+    half_sub[1]=~a&b;
+    end
+    endfunction
+  assign{borrow,diff}=half_sub(a,b);
+endmodule
+
+-----------------------------------------//structural level moding//-----------------------------------------------------
+module half_subtractor(
+  			input a,b,
+  			output diff,borrow
+);
+  wire term1,term2,term3;
+  not a1(not_a,a);
+  not a2(not_b,b);
+  and b1(term1,not_a,b);
+  and b2(term2,a,not_b);
+  or x1(diff,term1,term2);
+  and x2(borrow,not_a,b);
+endmodule
   
