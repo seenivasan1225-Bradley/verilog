86ade1f12d42eed0843850c8091ab94c2a26bab8
𝙎𝙀𝙀𝙉𝙄𝙑𝘼𝙎𝘼𝙉 <seenivasanv222@gmail.com>
2025-11-08 11:23:45 +0530

    Update Half_adder.v

--- assignment/HALF_ADDER/Half_adder.v
+++ assignment/HALF_ADDER/Half_adder.v
@@ -1,6 +1,4 @@
-//dataflow modeling//
-
-
+----------------------------------//1.dataflow modeling//---------------------------------------------
 module half_adder(
                  input a,b,
 		 output sum,carry
@@ -8,3 +6,21 @@ module half_adder(
    assign sum=a ^ b;
    assign carry=a & b;
 endmodule
+
+---------------------------//2.gate-level_modeling//---------------------------------------------------
+module half_adder(input a,b,
+                  output sum,carry);
+  
+  xor w1(sum,a,b);
+  and x1(carry,a,b);
+endmodule
+------------------------------------//3.behavioural modeling//----------------------------------------
+module half_adder(input a, b,
+                  output reg sum,carry);
+  
+  always@(*) begin
+    sum=a^b;
+    carry=a&b;
+  end
+endmodule
+  
