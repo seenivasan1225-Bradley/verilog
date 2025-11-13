6c6667b23430f49a333936a3014fe07d15b7afc7
𝙎𝙀𝙀𝙉𝙄𝙑𝘼𝙎𝘼𝙉 <seenivasanv222@gmail.com>
2025-11-13 12:29:57 +0530

    Update Half_adder.v

--- assignment/HALF_ADDER/Half_adder.v
+++ assignment/HALF_ADDER/Half_adder.v
@@ -23,4 +23,43 @@ module half_adder(input a, b,
     carry=a&b;
   end
 endmodule
+
+----------------------------------------------------//4.modeling using the switchcase//
+module half_adder(input a,b,
+                  output reg sum,carry);
+  always@(*) begin
+    case({a,b})
+      2'b00:
+      begin
+        sum=0;carry=0;
+      end
+      2'b01:
+      begin
+        sum=1;carry=0;
+      end
+      2'b10:
+        begin
+          sum=1;carry=0;
+        end
+      2'b11:
+        begin
+          sum=0;carry=1;
+        end
+    endcase
+  end
+endmodule 
+----------------------------------------------//5.functional level_modeling//--------------------------------------------------------------------------
+module half_adder(input a,b,
+                  output sum,carry);
+  
+  function[1:0]half_adder;
+    input a,b;
+    begin
+      half_adder[0]=a^b;
+      half_adder[1]=a&b;
+    end
+  endfunction
+  assign {carry,sum}=half_adder(a,b);
+endmodule
+   
   
